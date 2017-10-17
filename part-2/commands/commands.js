d305a1f33c5f5078da2e368da73a27564ec3de9c
James McCormack <thejamesmccormack@gmail.com>
2017-10-16 19:49:29 -0700

    tweaked database functions to print better with package

--- part-2/commands/commands.js
+++ part-2/commands/commands.js
@@ -1,38 +1,29 @@
+const print = require('node-print');
 const { allSectionProducts, allOrdersforShopper, listRealShoppers } = require('../database/database.js');
 
-// i'm getting an undefined result for my top row. possibly column headers? must look into further.
 const productList = (args) => {
-  console.log(`----------------------`);
-  console.log(`  Products   Section  `);
-  console.log(`----------------------`);
   allSectionProducts(args)
     .then((products) => {
-      products.forEach((product) => {
-        console.log(`| ${product.name} | ${product.section} |`);
-      });
+      print.pt(products);
     })
     .catch(console.error);
 };
 
-// console.log(productList('dairy'));
+console.log(productList('dairy'));
 
-// getting out the data i need, but also same undefined row up top. 
 const shopperOrders = (args) => {
-  console.log(`---------------------`);
-  console.log(` Order ID  Total Cost`);
-  console.log(`---------------------`);
   allOrdersforShopper(args)
     .then((orders) => {
-      orders.forEach((order) => {
-        console.log(`| ${order.order_id} | ${order.sum}`);
-      });
+      print.pt(orders);
     });
 };
 
-console.log(shopperOrders(1));
-
 const realShoppers = () => {
-  // future function coming
+  listRealShoppers()
+    .then((shoppers) => {
+      print.pt(shoppers);
+    })
+    .catch(console.error);
 };
 
 module.exports = { productList, shopperOrders, realShoppers };
--- part-2/database/database.js
+++ part-2/database/database.js
@@ -8,12 +8,10 @@ const connection = {
 
 const db = pgp(connection);
 
-// this one is complete.
 const allSectionProducts = (section) => {
-  return db.any(`SELECT * FROM grocery_items WHERE section=$1`, section);
+  return db.any(`SELECT name, section FROM grocery_items WHERE section=$1`, section);
 };
 
-// List all orders for a given shopper id, returns the order id, and the total cost of each order. This is the hardest one.
 const allOrdersforShopper = (shopperId) => {
   return db.any(`SELECT orders.order_id, SUM(grocery_items.price)
       FROM orders
@@ -26,7 +24,6 @@ const allOrdersforShopper = (shopperId) => {
       `, shopperId);
 };
 
-// List all the shoppers that have at least 1 order, and also display the number of orders for them.
 const listRealShoppers = () => {
   return db.any(`SELECT shoppers.shopper_name, COUNT(orders.order_id)
     FROM shoppers
