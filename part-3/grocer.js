d67e5bc5c9be30d79331d1dd7576ff5e35c9ef7c
James McCormack <thejamesmccormack@gmail.com>
2017-10-17 18:15:13 -0700

    got one button sending text to modal.

--- part-3/grocer.js
+++ part-3/grocer.js
@@ -4,22 +4,46 @@ document.addEventListener("DOMContentLoaded", () => {
   const cartButton = document.getElementById("cart-button");
   const modalCloseButton = document.querySelector('.modal-close-button');
   const cartCount = document.getElementById("cart-item-count");
+  const cartModal = document.querySelector('.modal-window');
 
   let cartCounter = parseInt(cartCount.innerText, 10);
   let modal = document.querySelector('.modal');
 
+  // utility functions for modal:
+
+  const openModal = () => {
+    modal.classList.add('modal-open');
+    document.body.classList.add('showing-modal');
+  };
+
+  const closeModal = () => {
+    modal.classList.remove('modal-open');
+    document.body.classList.remove('showing-modal');
+  }
+
   cartButton.addEventListener('click', (event) => {
     console.log('cart button clicked');
     event.preventDefault();
-    modal.classList.add('modal-open');
+    openModal();
   });
 
   modalCloseButton.addEventListener('click', (event) => {
     console.log('close button clicked');
-    modal.classList.remove('modal-open');
+    closeModal();
   })
 
-  // event listners for product buttons:
+  modal.addEventListener('click', (event) => {
+    if (event.target === modal) {
+      event.preventDefault();
+      closeModal();
+    }
+  });
+
+
+  // adding elements to the modal:
+  const cartList = document.querySelector('.cart-list');
+
+  // event listeners for product buttons:
   const onionButton = document.getElementById("onion-button");
   const tomatoButton = document.getElementById("tomato-button");
   const carrotButton = document.getElementById("carrot-button");
@@ -29,43 +53,47 @@ document.addEventListener("DOMContentLoaded", () => {
   const pizzaButton = document.getElementById("pizza-button");
 
   onionButton.addEventListener('click', (event) => {
-    console.log('onion button clicked');
     cartCounter += 1;
     cartCount.innerText = cartCounter;
+    const addItem = document.createElement('li');
+    addItem.className = 'listItem';
+    const itemName = event.target.previousElementSibling.previousElementSibling.innerText;
+    const nameSpan = document.createElement('span');
+    nameSpan.innerText = itemName;
+    addItem.appendChild(nameSpan);
+    const itemPrice = event.target.previousElementSibling.innerText;
+    const priceSpan = document.createElement('span');
+    priceSpan.innerText = itemPrice;
+    addItem.appendChild(priceSpan);
+    cartList.appendChild(addItem);
   });
 
   tomatoButton.addEventListener('click', (event) => {
-    console.log('tomato button clicked');
     cartCounter += 1;
     cartCount.innerText = cartCounter;
   });
 
   carrotButton.addEventListener('click', (event) => {
-    console.log('carrot button clicked');
     cartCounter += 1;
     cartCount.innerText = cartCounter;
   });
 
   coldcutButton.addEventListener('click', (event) => {
-    console.log('coldcut button clicked');
     cartCounter += 1;
     cartCount.innerText = cartCounter;
   });
 
   ojButton.addEventListener('click', (event) => {
-    console.log('oj button clicked');
     cartCounter += 1;
     cartCount.innerText = cartCounter;
   });
 
   icecreamButton.addEventListener('click', (event) => {
-    console.log('ice cream button clicked');
     cartCounter += 1;
     cartCount.innerText = cartCounter;
   });
 
   pizzaButton.addEventListener('click', (event) => {
-    console.log('pizza button clicked');
     cartCounter += 1;
     cartCount.innerText = cartCounter;
   });
